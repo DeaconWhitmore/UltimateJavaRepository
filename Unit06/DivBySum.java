2b5907bbd618335a258656ad8be65024b2f13d65
DeaconWhitmore <deaconwhitmore@gmail.com>
2024-03-18 09:57:36 -0500

    add progress check example

---
+++ Unit06/DivBySum.java
@@ -0,0 +1,9 @@
+public class DivBySum {
+    public static int divBySum(int[] arr, int num) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(divBySum(new int[] {4, 1, 3, 6, 2, 9}, 2));
+    }
+}
