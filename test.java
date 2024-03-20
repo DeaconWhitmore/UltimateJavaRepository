eae43832ff663540ccb42f623f3d7d0ff0b31c0f
DeaconWhitmore <deaconwhitmore@gmail.com>
2024-03-20 08:59:18 -0500

    test.java changes

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String[] args) {
+        int n = 5;
+        int a = 0;
+  for(int i = 0; i < n; i++){
+    System.out.print(a + "," + " ");
+    a++;
+    }
+}
+}
+    
